e9c0c4b3d316094ea43ae4b6bcd49728bfe0a2aa
Diogo Vicente <dioda-si@student.42.rio>
2025-10-24 21:03:27 -0300

    feat: add GestureDetector

--- maisum/lib/main.dart
+++ maisum/lib/main.dart
@@ -9,30 +9,44 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(      
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              margin: EdgeInsets.only(bottom: 10),
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(color: const Color.fromARGB(255, 88, 96, 30), borderRadius: BorderRadius.circular(15)),
-              child: Text(
-                'A simple text',
-                style: TextStyle(fontSize: 32, color: Colors.white),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(bottom: 10),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 88, 96, 30),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: const Text(
+                  'A simple text',
+                  style: TextStyle(fontSize: 32, color: Colors.white),
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(color: const Color.fromARGB(255, 239, 235, 223), borderRadius: BorderRadius.circular(25)),
-              child: Text(
-              'Click me',
-              style: TextStyle(fontSize: 18, color: Color.fromARGB(255, 91, 99, 31)),
-            ),
-            )
-          ],
+              GestureDetector(
+                onTap: () {
+                  print('Button pressed');
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 239, 235, 223),
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  child: const Text(
+                    'Click me',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Color.fromARGB(255, 91, 99, 31),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        )
+        ),
       ),
     );
   }
