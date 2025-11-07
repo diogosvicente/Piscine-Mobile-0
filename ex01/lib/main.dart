34c0472e5bd495c0acba8439d274e4ad06c092f2
Diogo Vicente <dioda-si@student.42.rio>
2025-11-07 19:47:55 -0300

    exercise: 01 done :white_check_mark:

---
+++ ex01/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+
+class _MyAppState extends State<MyApp> {
+  String texto = "A simple text";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(      
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(bottom: 10),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 88, 96, 30),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Text(
+                  texto,
+                  style: TextStyle(fontSize: 32, color: Colors.white),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: (){
+                  print("Click me");
+                  setState(() {
+                    texto = (texto == "A simple text") ? "Hello World" : "A simple text";
+                  });
+                  }, child: const Text(
+                    'Click me',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Color.fromARGB(255, 91, 99, 31),
+                  )
+                )
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
