e671f22282ef84e0ef6fc1ba850cbf7139bf82b6
Diogo Vicente <dioda-si@student.42.rio>
2025-11-07 19:06:44 -0300

    feat: ex00 done

--- ex00/lib/main.dart
+++ ex00/lib/main.dart
@@ -1,74 +1,41 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MaterialApp(home: Ex00()));
-}
+void main() => runApp(const MyApp());
 
-class Ex00 extends StatefulWidget {
-  const Ex00({super.key});
-
-  @override
-  State<Ex00> createState() => _Ex00State();
-}
-
-class _Ex00State extends State<Ex00> {
-  bool reversed = false;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Cria a lista de campos
-    final fields = [
-      const MyTextBox(label: 'Caixa 1'),
-      const MyTextBox(label: 'Caixa 2'),
-    ];
-
-    return Scaffold(
-      appBar: AppBar(title: const Text('Sem Key')),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: reversed ? fields.reversed.toList() : fields,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            reversed = !reversed; // inverte a ordem
-          });
-        },
-        child: const Icon(Icons.swap_horiz),
-      ),
-    );
-  }
-}
-
-class MyTextBox extends StatefulWidget {
-  final String label;
-  const MyTextBox({required this.label});
-
-  @override
-  State<MyTextBox> createState() => _MyTextBoxState();
-}
-
-class _MyTextBoxState extends State<MyTextBox> {
-  final controller = TextEditingController();
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(16),
-      width: 150,
-      child: TextField(
-        controller: controller,
-        decoration: InputDecoration(
-          labelText: widget.label,
-          border: const OutlineInputBorder(),
+    return MaterialApp(
+      home: Scaffold(      
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(bottom: 10),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 88, 96, 30),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: const Text(
+                  'A simple text',
+                  style: TextStyle(fontSize: 32, color: Colors.white),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: (){print("Click me");}, child: const Text(
+                    'Click me',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Color.fromARGB(255, 91, 99, 31),
+                  )
+                )
+              )
+            ],
+          ),
         ),
       ),
     );
